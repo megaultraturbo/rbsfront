6cc26eb3856aa72c817c103a7c8e934693622406
jakub <jakubwanat1@gmail.com>
2022-06-05 19:39:32 +0200

    test (fireship todo app)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useRef } from "react";
 
 function App() {
-  return (
-    //jsx - taki html ktory powstaje
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // state
+  const [todos, setTodos] = useState([]);
+
+  // binding
+  const todoText = useRef();
+
+  // side effects / lifecycle
+  useEffect(() => {
+    const existingTodos = localStorage.getItem('todos');
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+  } ,[]);
+
+  //events
+  function addTodo(event) {
+    event.preventDefault(); // strona sie nie odswieza przy submicie
+    const next = [...todos, todoText.current.value];
+    setTodos(next);
+    localStorage.setItem('todos', JSON.stringify(next));
+  }
+
+  // jsx - js syntax extension - to bedzie zwrocone na stronke jako html
+  return(
+    <div>
+      <ul>
+        {todos.map(todo => (<li key = {todo}>{todo}</li>))}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input ref={todoText}/>
+        <input type="submit" value="Add item"/>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
